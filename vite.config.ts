70a02947515952434679313ff0e997e4683f369a
winerar <m.winerar@gmail.com>
2026-01-28 19:09:36 +0400

    fix: set base path for deployment in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/katezhuk/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
