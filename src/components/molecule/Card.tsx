40f70433f262380d21d7acfd52853a36c9652243
winerar <m.winerar@gmail.com>
2026-02-05 19:50:24 +0400

    fix: remove unnecessary min-width from Card component for improved layout

--- src/components/molecule/Card.tsx
+++ src/components/molecule/Card.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 function Card({ children }: Props) {
   return (
-    <div className="min-w-100 flex-1 xl:max-w-xs">
+    <div className="flex-1 xl:max-w-xs">
       <Glass type="offset">
         <div className="text-platinum flex h-full flex-col justify-between p-6">
           {children}
