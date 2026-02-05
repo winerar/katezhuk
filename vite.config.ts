24f44b4857da9901fcc7369b16bcfec0fadd256b
winerar <m.winerar@gmail.com>
2026-02-05 15:34:11 +0400

    chore: update package.json and vite.config.ts for tailwindcss enhancements
    - Added "prepare" script to package.json for tw-patch installation.
    - Included "tailwindcss-patch" and "unplugin-tailwindcss-mangle" as dependencies.
    - Updated vite.config.ts to integrate unplugin-tailwindcss-mangle with existing plugins.

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import utwm from "unplugin-tailwindcss-mangle/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/katezhuk/",
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), utwm()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
-  }
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
