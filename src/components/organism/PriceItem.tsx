ba75a3de9c7c9f12bcbbe77f31484387b57a2593
winerar <m.winerar@gmail.com>
2026-03-27 16:22:23 +0400

    fix: update pricing structure in content.json and adjust PriceItem component for detailed pricing display

--- src/components/organism/PriceItem.tsx
+++ src/components/organism/PriceItem.tsx
@@ -4,9 +4,15 @@ import Card from "../molecule/Card";
 type Props = {
   title: string;
   description: React.ReactNode;
-  packages: { name: string; price: string }[];
+  packages: {
+    name: string;
+    price: {
+      regular: { amount: number; currency: string };
+      discounted?: { amount: number; currency: string };
+    };
+  }[];
 };
-
+//color-alabaster-grey
 function PriceItem({ title, description, packages }: Props) {
   return (
     <Card>
@@ -17,12 +23,27 @@ function PriceItem({ title, description, packages }: Props) {
       <div className="pt-6">
         {packages.map((pkg, index) => (
           <div key={index} className="flex items-center justify-between gap-4">
-            <Typography component="span" className="text-sm">
-              {pkg.name}
-            </Typography>
-            <Typography component="span" className="font-numerical text-2xl">
-              {pkg.price}
-            </Typography>
+            <Typography component="span">{pkg.name}</Typography>
+            {pkg.price.discounted ? (
+              <div className="flex items-center gap-4">
+                <Typography
+                  component="span"
+                  className="font-numerical text-alabaster-grey text-2xl line-through"
+                >
+                  {pkg.price.regular.amount} {pkg.price.regular.currency}
+                </Typography>
+                <Typography
+                  component="span"
+                  className="font-numerical text-2xl"
+                >
+                  {pkg.price.discounted.amount} {pkg.price.discounted.currency}
+                </Typography>
+              </div>
+            ) : (
+              <Typography component="span" className="font-numerical text-2xl">
+                {pkg.price.regular.amount} {pkg.price.regular.currency}
+              </Typography>
+            )}
           </div>
         ))}
       </div>
